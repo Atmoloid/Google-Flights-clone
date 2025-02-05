7c68b55d6a6fa129c6a7b51f356d84de28a8ea46
Lorenzo <lorenzopica31@gmail.com>
2025-02-05 15:17:19 +0100

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,14 @@ import { useState } from 'react'
 import ReactDOM from 'react-dom/client';
 import './styles.css'
 
+
 function App() {
-  return  <h1>ciao</h1>
+  return (
+    <div>
+      <h1>Ciao, React!</h1>
+    </div>
+  );
+ 
 }
 
-
-// Prendi il contenitore esistente nell'HTML
-const container = document.getElementById('flights-container');
-
-// Assicurati che il contenitore esista prima di creare il root
-if (container) {
-  ReactDOM.createRoot(container).render(<App />);
-} else {
-  console.error("Elemento #flights-container non trovato nel DOM");
-}
-console.log("cazzo");
-export default App
+export default App;
